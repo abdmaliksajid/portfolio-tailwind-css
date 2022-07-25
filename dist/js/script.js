24772bd910f41d6e74edb8497666a4916c778273
abdmaliksajid <abdmaliksajid@gmail.com>
2022-07-25 16:21:45 +0700

    Back to top coba

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#totop");
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 //akhir navbar
